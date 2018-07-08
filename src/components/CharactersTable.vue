bf95347c6b5fd26f758e51ab49d012cc6b6fb14c
Daniel Noonan <noonan_dr@yahoo.co.uk>
2018-07-08 22:58:35 +0100

    chore(vue): OM-002 removed and renamed default Vue generated files for project

---
+++ src/components/CharactersTable.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>Star Wars Characters</h1>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'CharactersTable',
+  props: {
+    msg: String
+  }
+}
+
+</script>
+
+
+<style scoped lang="scss">
+
+
+
+</style>
---
+++ src/views/Characters.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="om-characters">
+    <CharactersTable/>
+  </div>
+</template>
+
+<script>
+import CharactersTable from '@/components/CharactersTable.vue'
+
+export default {
+  name: 'home',
+  components: {
+    CharactersTable
+  }
+}
+</script>
