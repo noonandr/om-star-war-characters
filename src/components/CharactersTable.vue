b4e02b630936e196b8d3134399b6459bacbfcb35
Daniel Noonan <noonan_dr@yahoo.co.uk>
2018-07-09 02:43:44 +0100

    feat(character-table): OM-005 made character table an actual table with some of the values

--- src/components/CharactersTable.vue
+++ src/components/CharactersTable.vue
@@ -1,15 +1,38 @@
 <template>
-  <div class="hello">
-    <div v-for="(character, index) in characters" :key="character.id">
-
-      <div class="item">
-        <div class="details">
-          <div class="country"><span class="label">Planet name: </span><span class="value">{{ character.name }}</span></div>
-        </div>
-      </div>
-
-    </div>
-  </div>
+  <table align="center" style="width:100%">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Climate</th>
+        <th>Diameter</th>
+        <th>Gravity</th>
+        <th>Orbital Period</th>
+        <th>Population</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="character in characters" :key="character.name">
+        <td>
+          {{ character.name }}
+        </td>
+        <td>
+          {{ character.climate }}
+        </td>
+        <td>
+          {{ character.diameter }}
+        </td>
+        <td>
+          {{ character.gravity }}
+        </td>
+        <td>
+          {{ character.orbital_period }}
+        </td>
+        <td>
+          {{ character.population }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
 
 </div>
 </template>
